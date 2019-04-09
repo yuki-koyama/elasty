605b27b1957f732babe8c9eff0f2a26e86c59859
Yuki Koyama <yuki@koyama.xyz>
2019-04-09 19:47:33 +0900

    Add some structs and classes

--- include/elasty/elasty.hpp
+++ include/elasty/elasty.hpp
@@ -1,4 +1,36 @@
 #ifndef ELASTY_HPP
 #define ELASTY_HPP
 
+#include <memory>
+#include <vector>
+#include <Eigen/Core>
+
+namespace elasty
+{
+    struct Vertex
+    {
+        Eigen::Vector3d x;
+        Eigen::Vector3d v;
+    };
+
+    struct Constraint
+    {
+        virtual void calculateValue(double& C) = 0;
+        virtual void calculateGrad(Eigen::Matrix3Xd& grad_C) = 0;
+    };
+
+    class Engine
+    {
+    public:
+
+        virtual void initializeScene() = 0;
+        virtual void stepTime() = 0;
+        virtual void addConstraint(std::shared_ptr<Constraint> constraint) = 0;
+
+    private:
+
+        std::vector<std::shared_ptr<Constraint>> m_constraints;
+    };
+}
+
 #endif // ELASTY_HPP
---
+++ src/elasty.cpp
@@ -0,0 +1,1 @@
+#include <elasty/elasty.hpp>
