8200e1114fd6718fd1cdc53cc8250b42061b1cf9
Yuki Koyama <yuki@koyama.xyz>
2019-04-12 21:07:45 +0900

    Implement environmental collision constraints and update engine definition

--- include/elasty/elasty.hpp
+++ include/elasty/elasty.hpp
@@ -1,23 +1,21 @@
 #ifndef ELASTY_HPP
 #define ELASTY_HPP
 
+#include <cassert>
 #include <memory>
 #include <vector>
 #include <Eigen/Core>
 
 namespace elasty
 {
-    struct Vertex
+    class Constraint;
+
+    struct Particle
     {
         Eigen::Vector3d x;
         Eigen::Vector3d v;
+        Eigen::Vector3d p;
         double m;
-    };
-
-    struct Constraint
-    {
-        virtual void calculateValue(double& C) = 0;
-        virtual void calculateGrad(Eigen::Matrix3Xd& grad_C) = 0;
     };
 
     class Engine
@@ -26,14 +24,70 @@ namespace elasty
 
         virtual void initializeScene() = 0;
         virtual void stepTime() = 0;
+
         virtual void addConstraint(std::shared_ptr<Constraint> constraint)
         {
             m_constraints.push_back(constraint);
         }
 
-    private:
+        virtual void addInstantConstraint(std::shared_ptr<Constraint> constraint)
+        {
+            m_instant_constraints.push_back(constraint);
+        }
+
+        std::vector<Particle> m_particles;
 
         std::vector<std::shared_ptr<Constraint>> m_constraints;
+        std::vector<std::shared_ptr<Constraint>> m_instant_constraints;
+
+    protected:
+
+        void projectConstraint(std::shared_ptr<Constraint> constraint);
+    };
+
+    class Constraint
+    {
+    public:
+        Constraint(const Engine* engine, const std::vector<unsigned int>& indices) :
+        m_engine(engine),
+        m_indices(indices)
+        {
+        }
+
+        virtual double calculateValue() = 0;
+        virtual Eigen::VectorXd calculateGrad() = 0;
+
+        const Engine* m_engine;
+        const std::vector<unsigned int> m_indices;
+    };
+
+    class EnvironmentalCollisionConstraint final : public Constraint
+    {
+    public:
+
+        EnvironmentalCollisionConstraint(const Engine* engine, const std::vector<unsigned int>& indices, const Eigen::Vector3d& n, const double d) :
+        Constraint(engine, indices),
+        m_n(n),
+        m_d(d)
+        {
+            assert(m_indices.size() == 1);
+        }
+
+        double calculateValue() override
+        {
+            const Eigen::Vector3d& x = m_engine->m_particles[m_indices[0]].p;
+            return m_n.transpose() * x - m_d;
+        }
+
+        Eigen::VectorXd calculateGrad() override
+        {
+            return m_n;
+        }
+
+    private:
+
+        const Eigen::Vector3d m_n;
+        const double m_d;
     };
 }
 
--- src/elasty.cpp
+++ src/elasty.cpp
@@ -1,1 +1,34 @@
 #include <elasty/elasty.hpp>
+
+void elasty::Engine::projectConstraint(std::shared_ptr<Constraint> constraint)
+{
+    // Calculate $C$
+    const double C = constraint->calculateValue();
+
+    // Calculate $\Nabla C$
+    const Eigen::VectorXd grad_C = constraint->calculateGrad();
+
+    // Calculate $\mathbf{M}^{-1}$
+    const unsigned int n = constraint->m_indices.size();
+    std::vector<double> inverse_mass_raw(3 * n);
+    for (unsigned int j = 0; j < n; ++ j)
+    {
+        inverse_mass_raw[j + 0] = 1.0 / m_particles[constraint->m_indices[j]].m;
+        inverse_mass_raw[j + 1] = 1.0 / m_particles[constraint->m_indices[j]].m;
+        inverse_mass_raw[j + 2] = 1.0 / m_particles[constraint->m_indices[j]].m;
+    }
+    const auto inverse_M_diagnal = Eigen::Map<Eigen::VectorXd>(inverse_mass_raw.data(), 3 * n);
+
+    // Calculate $s$
+    const double s = C / (grad_C.transpose() * inverse_M_diagnal.asDiagonal() * grad_C);
+
+    // Calculate $\Delta x$
+    const Eigen::VectorXd delta_x = - s * inverse_M_diagnal.asDiagonal() * grad_C;
+
+    // Update predicted positions
+    for (unsigned int j = 0; j < n; ++ j)
+    {
+        m_particles[constraint->m_indices[j]].p += delta_x;
+    }
+}
+
