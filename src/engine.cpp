916db1c6cc45ecca2c133d4d7add7aebd5abf709
Yuki Koyama <yuki@koyama.xyz>
2019-04-13 17:18:31 +0900

    Fix a bug on the multiple-particle constraints handling

--- src/engine.cpp
+++ src/engine.cpp
@@ -70,9 +70,9 @@ void elasty::Engine::projectConstraint(std::shared_ptr<Constraint> constraint)
     std::vector<double> inverse_mass_raw(3 * n);
     for (unsigned int j = 0; j < n; ++ j)
     {
-        inverse_mass_raw[j + 0] = 1.0 / m_particles[constraint->m_indices[j]].m;
-        inverse_mass_raw[j + 1] = 1.0 / m_particles[constraint->m_indices[j]].m;
-        inverse_mass_raw[j + 2] = 1.0 / m_particles[constraint->m_indices[j]].m;
+        inverse_mass_raw[j * 3 + 0] = 1.0 / m_particles[constraint->m_indices[j]].m;
+        inverse_mass_raw[j * 3 + 1] = 1.0 / m_particles[constraint->m_indices[j]].m;
+        inverse_mass_raw[j * 3 + 2] = 1.0 / m_particles[constraint->m_indices[j]].m;
     }
     const auto inverse_M_diagnal = Eigen::Map<Eigen::VectorXd>(inverse_mass_raw.data(), 3 * n);
 
