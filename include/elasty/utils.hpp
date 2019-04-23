816717d6ff2f099d154e0518a750512ae1eaecde
Yuki Koyama <yuki@koyama.xyz>
2019-04-23 20:10:20 +0900

    Add interfaces for exporting alembic data

--- include/elasty/utils.hpp
+++ include/elasty/utils.hpp
@@ -1,6 +1,7 @@
 #ifndef elasty_utils_hpp
 #define elasty_utils_hpp
 
+#include <string>
 #include <vector>
 #include <Eigen/Core>
 
@@ -8,11 +9,18 @@ namespace elasty
 {
     struct Particle;
     class Constraint;
+    class ClothSimObject;
+    class AlembicManager;
 
     void generateFixedPointConstraints(const Eigen::Vector3d& search_position,
                                        const Eigen::Vector3d& fixed_position,
                                        const std::vector<std::shared_ptr<Particle>>& particles,
                                        std::vector<std::shared_ptr<Constraint>>& constraints);
+
+    std::shared_ptr<AlembicManager> createAlembicManager(const std::string& file_path,
+                                                         const std::shared_ptr<ClothSimObject> cloth_sim_object);
+
+    void submitCurrentStatus(const std::shared_ptr<AlembicManager> alembic_manager);
 }
 
 #endif /* elasty_utils_hpp */
