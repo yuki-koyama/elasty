bbb629a2780bad5f0ee3b0e71b95aea349a69ffc
Yuki Koyama <yuki@koyama.xyz>
2019-04-24 09:13:48 +0900

    Add tentative implementation of alembic-related functions

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,9 +3,9 @@
 #include <elasty/particle.hpp>
 
 void elasty::generateFixedPointConstraints(const Eigen::Vector3d& search_position,
-                                          const Eigen::Vector3d& fixed_position,
-                                          const std::vector<std::shared_ptr<Particle>>& particles,
-                                          std::vector<std::shared_ptr<Constraint>>& constraints)
+                                           const Eigen::Vector3d& fixed_position,
+                                           const std::vector<std::shared_ptr<Particle>>& particles,
+                                           std::vector<std::shared_ptr<Constraint>>& constraints)
 {
     for (const auto& particle : particles)
     {
@@ -14,4 +14,21 @@ void elasty::generateFixedPointConstraints(const Eigen::Vector3d& search_positio
             constraints.push_back(std::make_shared<elasty::FixedPointConstraint>(particle, 1.0, fixed_position));
         }
     }
+}
+
+class elasty::AlembicManager
+{
+    // TODO
+};
+
+std::shared_ptr<elasty::AlembicManager> elasty::createAlembicManager(const std::string& file_path,
+                                                                     const std::shared_ptr<ClothSimObject> cloth_sim_object)
+{
+    // TODO
+    return nullptr;
+}
+
+void elasty::submitCurrentStatus(const std::shared_ptr<AlembicManager> alembic_manager)
+{
+    // TODO
 }
