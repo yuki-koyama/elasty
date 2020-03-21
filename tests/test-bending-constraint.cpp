85e3ca0c00d4307ae94e17b6ffdf60943248b9de
Yuki Koyama <yuki@koyama.xyz>
2020-03-21 13:32:56 +0900

    Use using declarations

--- tests/test-bending-constraint.cpp
+++ tests/test-bending-constraint.cpp
@@ -2,26 +2,29 @@
 #include <elasty/particle.hpp>
 #include <gtest/gtest.h>
 
+using Eigen::Matrix;
+using Eigen::Vector3d;
+
 TEST(CostraintTest, BendingZero)
 {
     constexpr double dt = 1.0 / 60.0;
 
-    auto p_0 = std::make_shared<elasty::Particle>(Eigen::Vector3d(0.0, 0.0, 0.0), Eigen::Vector3d::Zero(), 1.0);
-    auto p_1 = std::make_shared<elasty::Particle>(Eigen::Vector3d(0.0, 1.0, 0.0), Eigen::Vector3d::Zero(), 1.0);
-    auto p_2 = std::make_shared<elasty::Particle>(Eigen::Vector3d(-0.5, 0.5, 0.0), Eigen::Vector3d::Zero(), 1.0);
-    auto p_3 = std::make_shared<elasty::Particle>(Eigen::Vector3d(+0.5, 0.5, 0.0), Eigen::Vector3d::Zero(), 1.0);
+    auto p_0 = std::make_shared<elasty::Particle>(Vector3d(0.0, 0.0, 0.0), Vector3d::Zero(), 1.0);
+    auto p_1 = std::make_shared<elasty::Particle>(Vector3d(0.0, 1.0, 0.0), Vector3d::Zero(), 1.0);
+    auto p_2 = std::make_shared<elasty::Particle>(Vector3d(-0.5, 0.5, 0.0), Vector3d::Zero(), 1.0);
+    auto p_3 = std::make_shared<elasty::Particle>(Vector3d(+0.5, 0.5, 0.0), Vector3d::Zero(), 1.0);
 
-    const Eigen::Vector3d& x_0 = p_0->x;
-    const Eigen::Vector3d& x_1 = p_1->x;
-    const Eigen::Vector3d& x_2 = p_2->x;
-    const Eigen::Vector3d& x_3 = p_3->x;
+    const Vector3d& x_0 = p_0->x;
+    const Vector3d& x_1 = p_1->x;
+    const Vector3d& x_2 = p_2->x;
+    const Vector3d& x_3 = p_3->x;
 
-    const Eigen::Vector3d p_10 = x_1 - x_0;
-    const Eigen::Vector3d p_20 = x_2 - x_0;
-    const Eigen::Vector3d p_30 = x_3 - x_0;
+    const Vector3d p_10 = x_1 - x_0;
+    const Vector3d p_20 = x_2 - x_0;
+    const Vector3d p_30 = x_3 - x_0;
 
-    const Eigen::Vector3d n_0 = p_10.cross(p_20).normalized();
-    const Eigen::Vector3d n_1 = p_10.cross(p_30).normalized();
+    const Vector3d n_0 = p_10.cross(p_20).normalized();
+    const Vector3d n_1 = p_10.cross(p_30).normalized();
 
     EXPECT_FALSE(n_0.hasNaN());
     EXPECT_FALSE(n_1.hasNaN());
@@ -35,7 +38,7 @@ TEST(CostraintTest, BendingZero)
 
     const double value = constraint->calculateValue();
 
-    Eigen::Matrix<double, 12, 1> grad;
+    Matrix<double, 12, 1> grad;
     constraint->calculateGrad(grad.data());
 
     constexpr double epsilon = 1e-20;
@@ -48,10 +51,10 @@ TEST(ConstraintTest, BendingDerivative)
 {
     constexpr double dt = 1.0 / 60.0;
 
-    auto p_0 = std::make_shared<elasty::Particle>(Eigen::Vector3d(0.0, 0.0, 0.0), Eigen::Vector3d::Zero(), 1.0);
-    auto p_1 = std::make_shared<elasty::Particle>(Eigen::Vector3d(0.0, 1.0, 0.0), Eigen::Vector3d::Zero(), 1.0);
-    auto p_2 = std::make_shared<elasty::Particle>(Eigen::Vector3d(-0.5, 0.5, 0.0), Eigen::Vector3d::Zero(), 1.0);
-    auto p_3 = std::make_shared<elasty::Particle>(Eigen::Vector3d(+0.5, 0.5, 0.0), Eigen::Vector3d::Zero(), 1.0);
+    auto p_0 = std::make_shared<elasty::Particle>(Vector3d(0.0, 0.0, 0.0), Vector3d::Zero(), 1.0);
+    auto p_1 = std::make_shared<elasty::Particle>(Vector3d(0.0, 1.0, 0.0), Vector3d::Zero(), 1.0);
+    auto p_2 = std::make_shared<elasty::Particle>(Vector3d(-0.5, 0.5, 0.0), Vector3d::Zero(), 1.0);
+    auto p_3 = std::make_shared<elasty::Particle>(Vector3d(+0.5, 0.5, 0.0), Vector3d::Zero(), 1.0);
 
     const std::shared_ptr<elasty::Particle> particles[] = {p_0, p_1, p_2, p_3};
 
@@ -69,8 +72,8 @@ TEST(ConstraintTest, BendingDerivative)
     {
         for (int j = 0; j < 3; ++j)
         {
-            Eigen::Vector3d eps3d = Eigen::Vector3d::Zero();
-            eps3d(j)              = eps;
+            Vector3d eps3d = Vector3d::Zero();
+            eps3d(j)       = eps;
 
             particles[i]->p        = particles[i]->x + eps3d;
             const double cost_plus = isometric_bending_constraint->calculateValue();
@@ -82,7 +85,7 @@ TEST(ConstraintTest, BendingDerivative)
 
             const double numerical_derivative = (cost_plus - cost_minus) / (2.0 * eps);
 
-            Eigen::Matrix<double, 12, 1> grad;
+            Matrix<double, 12, 1> grad;
             isometric_bending_constraint->calculateGrad(grad.data());
             const double analytic_derivative = grad(i * 3 + j);
 
