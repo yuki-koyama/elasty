d4797a9e543d3fc8e953ef52e99e9c67198dd1a5
Yuki Koyama <yuki@koyama.xyz>
2020-03-28 18:21:02 +0900

    Move to .cpp file

--- include/elasty/utils.hpp
+++ include/elasty/utils.hpp
@@ -3,10 +3,7 @@
 
 #include <Eigen/Core>
 #include <memory>
-#include <sstream>
 #include <string>
-#include <unordered_map>
-#include <utility>
 #include <vector>
 
 namespace elasty
@@ -42,61 +39,10 @@ namespace elasty
 
     void submitCurrentStatus(const std::shared_ptr<AlembicManager> alembic_manager);
 
-    inline std::string generateClothMeshObjData(const double   width                 = 1.0,
-                                                const double   height                = 1.0,
-                                                const unsigned horizontal_resolution = 10,
-                                                const unsigned vertical_resolution   = 10)
-    {
-        std::vector<Eigen::Vector3d> vertices;
-        std::vector<Eigen::Vector3i> triangles;
-
-        const double h_step = width / static_cast<double>(horizontal_resolution + 1);
-        const double v_step = height / static_cast<double>(vertical_resolution + 1);
-
-        // Vertices
-        for (unsigned h_index = 0; h_index <= horizontal_resolution; ++h_index)
-        {
-            for (unsigned v_index = 0; v_index <= vertical_resolution; ++v_index)
-            {
-                const double x = [&]() {
-                    if (v_index % 2 == 0 || h_index == 0)
-                    {
-                        return static_cast<double>(h_index) * h_step - 0.5 * width;
-                    }
-                    else
-                    {
-                        return (static_cast<double>(h_index) - 0.5) * h_step - 0.5 * width;
-                    }
-                }();
-                const double y = static_cast<double>(v_index) * v_step - 0.5 * height;
-
-                vertices.push_back(Eigen::Vector3d(x, y, 0.0));
-
-                // Additional vetex at the even-indexed row
-                if (v_index % 2 == 0 || h_index == horizontal_resolution)
-                {
-                    vertices.push_back(Eigen::Vector3d(0.5 * width, y, 0.0));
-                }
-            }
-        }
-
-        // Triangles
-        // TODO
-
-        // Output
-        std::stringstream sstream;
-        for (const auto& vertex : vertices)
-        {
-            sstream << "v " << vertex(0) << " " << vertex(1) << " " << vertex(2) << std::endl;
-        }
-        for (const auto& triangle : triangles)
-        {
-            sstream << "f " << triangle(0) << " " << triangle(1) << " " << triangle(2) << std::endl;
-        }
-
-        return sstream.str();
-    }
-
+    std::string generateClothMeshObjData(const double   width                 = 1.0,
+                                         const double   height                = 1.0,
+                                         const unsigned horizontal_resolution = 10,
+                                         const unsigned vertical_resolution   = 10);
 } // namespace elasty
 
 #endif // ELASTY_UTILS_HPP
--- src/utils.cpp
+++ src/utils.cpp
@@ -4,6 +4,9 @@
 #include <elasty/constraint.hpp>
 #include <elasty/particle.hpp>
 #include <elasty/utils.hpp>
+#include <sstream>
+#include <unordered_map>
+#include <utility>
 
 std::vector<float> elasty::packParticlePositions(const std::vector<std::shared_ptr<Particle>>& particles)
 {
@@ -131,4 +134,59 @@ elasty::createAlembicManager(const std::string&                    file_path,
 void elasty::submitCurrentStatus(const std::shared_ptr<AlembicManager> alembic_manager)
 {
     alembic_manager->submitCurrentStatus();
+}
+
+std::string elasty::generateClothMeshObjData(const double   width,
+                                             const double   height,
+                                             const unsigned horizontal_resolution,
+                                             const unsigned vertical_resolution)
+{
+    std::vector<Eigen::Vector3d> vertices;
+    std::vector<Eigen::Vector3i> triangles;
+
+    const double h_step = width / static_cast<double>(horizontal_resolution + 1);
+    const double v_step = height / static_cast<double>(vertical_resolution + 1);
+
+    // Vertices
+    for (unsigned h_index = 0; h_index <= horizontal_resolution; ++h_index)
+    {
+        for (unsigned v_index = 0; v_index <= vertical_resolution; ++v_index)
+        {
+            const double x = [&]() {
+                if (v_index % 2 == 0 || h_index == 0)
+                {
+                    return static_cast<double>(h_index) * h_step - 0.5 * width;
+                }
+                else
+                {
+                    return (static_cast<double>(h_index) - 0.5) * h_step - 0.5 * width;
+                }
+            }();
+            const double y = static_cast<double>(v_index) * v_step - 0.5 * height;
+
+            vertices.push_back(Eigen::Vector3d(x, y, 0.0));
+
+            // Additional vetex at the even-indexed row
+            if (v_index % 2 == 0 || h_index == horizontal_resolution)
+            {
+                vertices.push_back(Eigen::Vector3d(0.5 * width, y, 0.0));
+            }
+        }
+    }
+
+    // Triangles
+    // TODO
+
+    // Output
+    std::stringstream sstream;
+    for (const auto& vertex : vertices)
+    {
+        sstream << "v " << vertex(0) << " " << vertex(1) << " " << vertex(2) << std::endl;
+    }
+    for (const auto& triangle : triangles)
+    {
+        sstream << "f " << triangle(0) << " " << triangle(1) << " " << triangle(2) << std::endl;
+    }
+
+    return sstream.str();
 }
