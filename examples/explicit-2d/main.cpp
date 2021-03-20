263400aad7b67966d394d3e42856d2191cb7fd5c
Yuki Koyama <yuki@koyama.xyz>
2021-03-21 00:07:53 +0900

    Add references into code comments

--- examples/explicit-2d/main.cpp
+++ examples/explicit-2d/main.cpp
@@ -13,7 +13,6 @@ namespace
     constexpr double k_youngs_modulus = 100.0;
     constexpr double k_poisson_ratio  = 0.45;
 
-    // Reference: https://encyclopediaofmath.org/wiki/Lam%C3%A9_constants
     constexpr double k_first_lame  = elasty::fem::calcFirstLame(k_youngs_modulus, k_poisson_ratio);
     constexpr double k_second_lame = elasty::fem::calcSecondLame(k_youngs_modulus, k_poisson_ratio);
 } // namespace
--- include/elasty/fem.hpp
+++ include/elasty/fem.hpp
@@ -3,13 +3,25 @@
 
 #include <Eigen/Core>
 
+// References:
+//
+// [1] Eftychios Sifakis and Jernej Barbic. 2012. FEM simulation of 3D deformable solids: a practitioner's guide to
+// theory, discretization and model reduction. In ACM SIGGRAPH 2012 Courses (SIGGRAPH '12). Association for Computing
+// Machinery, New York, NY, USA, Article 20, 1–50. DOI:https://doi.org/10.1145/2343483.2343501
+//
+// [2] Theodore Kim and David Eberle. 2020. Dynamic deformables: implementation and production practicalities. In ACM
+// SIGGRAPH 2020 Courses (SIGGRAPH '20). Association for Computing Machinery, New York, NY, USA, Article 23, 1–182.
+// DOI:https://doi.org/10.1145/3388769.3407490
+
 namespace elasty::fem
 {
+    // Reference: [1]
     template <typename Scalar> constexpr Scalar calcFirstLame(const Scalar youngs_modulus, const Scalar poisson_ratio)
     {
         return youngs_modulus * poisson_ratio / ((1.0 + poisson_ratio) * (1.0 - 2.0 * poisson_ratio));
     }
 
+    // Reference: [1]
     template <typename Scalar> constexpr Scalar calcSecondLame(const Scalar youngs_modulus, const Scalar poisson_ratio)
     {
         return youngs_modulus / (2.0 * (1.0 + poisson_ratio));
