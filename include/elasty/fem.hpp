7e9f45cac34323746e86a3c441787fa272c2ede0
Yuki Koyama <yuki@koyama.xyz>
2021-03-20 17:05:58 +0900

    Add functions to calculate lame coefficients

--- include/elasty/fem.hpp
+++ include/elasty/fem.hpp
@@ -3,9 +3,17 @@
 
 #include <Eigen/Core>
 
-namespace elasty
+namespace elasty::fem
 {
+    template <typename Scalar> constexpr Scalar calcFirstLame(const Scalar youngs_modulus, const Scalar poisson_ratio)
+    {
+        return youngs_modulus * poisson_ratio / ((1.0 + poisson_ratio) * (1.0 - 2.0 * poisson_ratio));
+    }
 
-} // namespace elasty
+    template <typename Scalar> constexpr Scalar calcSecondLame(const Scalar youngs_modulus, const Scalar poisson_ratio)
+    {
+        return youngs_modulus / (2.0 * (1.0 + poisson_ratio));
+    }
+} // namespace elasty::fem
 
 #endif // ELASTY_FEM_HPP
