e49028680bf9da492cc513c841313ac2d01777d4
Yuki Koyama <yuki@koyama.xyz>
2021-04-09 00:43:30 +0900

    Add a new source to test FEM functions

---
+++ tests/fem-test.cpp
@@ -0,0 +1,19 @@
+#include <elasty/fem.hpp>
+#include <gtest/gtest.h>
+
+TEST(FemTest, StVenantKirchhoff2d)
+{
+    // TODO
+}
+
+TEST(FemTest, CoRotational2d)
+{
+    // TODO
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
