f717b785c8d1d51da7b0f6e2f2e2d37ae1069b88
Yuki Koyama <yuki@koyama.xyz>
2021-05-23 18:34:42 +0900

    Remove debug lines

--- .github/workflows/clang-format.sh
+++ .github/workflows/clang-format.sh
@@ -1,4 +1,2 @@
-clang-format src/*.cpp include/elasty/*.hpp tests/*.*pp examples/**/*.*pp --output-replacements-xml --verbose
-clang-format src/*.cpp include/elasty/*.hpp tests/*.*pp examples/**/*.*pp --verbose
 clang-format src/*.cpp include/elasty/*.hpp tests/*.*pp examples/**/*.*pp --output-replacements-xml | grep -c "<replacement " >/dev/null
 if [ $? -ne 1 ]; then echo "Ill-styled code detected" && exit 1; fi
